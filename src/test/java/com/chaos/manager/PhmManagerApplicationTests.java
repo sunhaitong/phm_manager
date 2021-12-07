f4458643c8207cb9a9216b9992066fff3558f14c
sunhaitong <779603582@qq.com>
2021-12-07 10:31:20 +0800

    init

---
+++ src/test/java/com/chaos/manager/PhmManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaos.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhmManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
