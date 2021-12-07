49087659cc80b4ae9ee89f32f91f32a59f0547b0
sunhaitong <779603582@qq.com>
2021-12-07 13:57:40 +0800

    初始化程序

--- src/main/java/com/chaos/manager/PhmManagerApplication.java
+++ src/main/java/com/chaos/manager/PhmManagerApplication.java
@@ -1,10 +1,13 @@
 package com.chaos.manager;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PhmManagerApplication {
+@Slf4j
+public class PhmManagerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PhmManagerApplication.class, args);
